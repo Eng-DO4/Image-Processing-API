777fc76f878357e9843227983c8154de152fb0a4
Eng-DO4 <mustafayasser1212@gmail.com>
2022-11-21 21:01:57 +0200

    tests are now ready

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -3,9 +3,25 @@ import app from '../server';
 
 const request = supertest(app);
 
-// bla bla bla
+// test main end point
 describe('Testing main endpoint', () => {
   it('The main endpoint always does not return errors', async () => {
     await request.get('/').expect(200);
   });
 });
+
+// test resizing endpoint
+describe('Testing resizing endpoint', () => {
+  it('No params provided', async () => {
+    await request.get('/resize').expect(400);
+  });
+  it('Not existing image', async () => {
+    await request.get('/resize?name=dr4').expect(400);
+  });
+  it('Width or height or both not provided', async () => {
+    await request.get('/resize?name=fjord').expect(400);
+  });
+  it('Resized image successfully', async () => {
+    await request.get('/resize?name=fjord&width=100&height=100').expect(200);
+  });
+})
